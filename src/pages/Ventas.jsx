5c93afdc23bbc7a2781bb535f15ce8e95c5876a6
skulltom666 <106767254+skulltom666@users.noreply.github.com>
2025-12-02 18:05:01 -0500

    Fix import path for MetodoPago component

--- src/pages/Ventas.jsx
+++ src/pages/Ventas.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import MetodoPago from "../Components/MetodoPago";
+import MetodoPago from "../components/MetodoPago";
 import { ShoppingCart } from "lucide-react";
 
 export default function Ventas() {
